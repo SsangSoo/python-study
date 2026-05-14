d99b3420e91e485b5ca8022900ed166c11d4196a
SsangSoo <kss10080@naver.com>
2026-05-14 23:08:51 +0900

    python 입문 chapter05 - 반복문 for문 실습
    range 함수(시작/끝/step), for in 구문, 1~1000 합 계산, Iterables 자료형(문자열, 리스트, 튜플, 집합, 딕셔너리) 반복 학습
    챕터 구조 재정리(chapter02→03, chapter03→04, chapter04→05), README 강의 목록 추가

---  introduction/chapter03/02.py
+++  introduction/chapter03/02.py
@@ -1,122 +1,100 @@
-# 파이썬 문자형
-# 문자형
+# 파이썬 완전 기초
+# 파이썬 변수
 
-# 문자열 생성
-str1 = "I am Python"
-str2 = "Python"
-str3 = """How are you?"""
-str4 = '''Thank you!'''
-
-print(type(str1), type(str2), type(str3), type(str4))
-print(len(str1), len(str2), len(str3), len(str4))
-
-
-# 빈 문자열
-str1_t1 = ''
-str2_t2 = str()
-
-print(type(str1_t1), len(str1_t1))
-print(type(str2_t2), len(str2_t2))
-
-# 이스케이프 문자 사용
-"""
-참고 : Escape 코드
-
-\n : 개행
-\t : 탭
-\\ : 문자
-\' : 문자
-\" : 문자
-\000 : 널 문자
-...
-"""
-
-# I'm Boy
-print("I'm Boy")
-print('I\'m Boy')
-print('a \t b')
-print('a \"\" b')
-
-escape_str1 = "Do you have a \"retro games\"?"
-print(escape_str1)
-escape_str2 = 'What\'s on TV?'
-print(escape_str2)
-
-# 탭, 줄 바꿈
-t_s1 = "Click \t Start!"
-t_s2 = "New Line \n Check!"
-
-print(t_s1)
-print(t_s2)
-print()
-
-# Raw String 출력
-raw_s1 = r'D:\Python\test'
-print(raw_s1)
-print()
-
-# 멀티라인 입력
-# 역슬래시 사용
-multi_str = \
-'''
-String
-Multi line
-Test
-'''
-
-print(multi_str)
-
-# 문자열 연산
-str_o1 = "Python"
-str_o2 = "Apple!"
-str_o3 = "How are you doing"
-str_o4 = "Seoul Daejeon Busan Jinju"
-
-print(str_o1 * 3)
-print(str_o1 + str_o2)
-print('y' in str_o1)
-print('n' in str_o1)
-print('p' not in str_o2)
-
-# 문자열 형 변환
-print(str(66), type(str(66)))
-print(str(10.1))
-print(str(True), type(str(True)))
-
-# 문자열 함수(upper, isalnum, startswith, count, endswith, isalpha...)
-print("Capitalize: ", str_o1.capitalize())
-print("endswith?: ", str_o2.endswith("!"))
-print("replace", str_o1.replace("thon", ' Good'))
-print("sorted: ", sorted(str_o1))
-print("split: ", str_o4.split(' '))
-
-
-# 반복(시퀀스)
-im_str = "Good Boy!"
-
-print(dir(im_str)) # __iter__
+# 기본 선언
+n = 700
 
 # 출력
-for i in im_str:
-    print(i)
+print(n)
+print(type(n))
 
-# 슬라이싱
-str_s1 = "Nice Python"
+# 동시 선언
+x = y = z = 700
+print(x, y, z)
 
-# 슬라이싱 연습
-print(str_s1[0:3]) # 0 1 2
-print(str_s1[5:]) # [5:11]
-print(str_s1[:len(str_s1)]) # str_s1[:11]
-print(str_s1[:len(str_s1)-1]) # str_s1[:10]
-print(str_s1[1:9:2])
-print(str_s1[-5:])
-print(str_s1[1:-2])
-print(str_s1[::2])
-print(str_s1[::-1])
 
-# 아스키 코드(또는 유니코드)
-a = 'z'
+# 선언
+var = 75
 
-print(ord(a)) # 아스키 코드로
-print(chr(122)) # 문자로
+# 재선언
+var = "Change Value"
 
+
+# 출력
+print(var)
+print(type(var))
+
+# Object References
+# 변수 값 할당 상태
+# 1. 타입에 맞는 오브젝트 생성
+# 2. 값 생성
+# 3. 콘솔 출력
+
+# 예1)
+print(300)
+print(int(300))
+
+# 예2)
+# n -> 777
+n = 777
+
+print(n, type(n))
+print()
+
+m = n
+# m -> 777 <- n
+print(m, n)
+print(type(m), type(n))
+print()
+
+m = 400
+print(m, n)
+print(type(m), type(n))
+print()
+
+
+# id(identity) 확인 : 객체의 고유값 확인
+m = 800
+n = 655
+
+print(id(m))
+print(id(n))
+print(id(m)==id(n))
+print()
+
+# 같은 오브젝트 참조
+m = 800
+n = 800
+
+print(id(m))
+print(id(n))
+print(id(m) == id(n))
+
+# 다양한 변수 선언
+# Camel Case : numberOfCollegeGraduates -> Method
+# Pascal Case : NumberOfCollegeGraduates -> Class
+# Snake Case : number_of_college_graduates
+
+# 허용하는 변수 선언 법
+age = 1
+Age = 2
+aGe = 3
+AGE = 4
+a_g_e = 5
+_age = 6
+age_ = 7
+_AGE_ = 7
+
+
+# 예약어는 변수명으로 불가능
+"""
+False def if raise
+None del import return
+True elif in try
+and else is while
+as except lambda with
+assert finally nonlocal yield
+break for not
+class form or
+continue global pass
+"""
---  introduction/chapter04/02.py
+++  introduction/chapter04/02.py
@@ -1,42 +1,122 @@
-# 파이썬 반복문
-# FOR 실습
+# 파이썬 문자형
+# 문자형
 
-# 코딩의 핵심
-# for in <collection>
-#   <Loop body>
+# 문자열 생성
+str1 = "I am Python"
+str2 = "Python"
+str3 = """How are you?"""
+str4 = '''Thank you!'''
 
-for v1 in range(10): # 0 ~ 9
-    print('v1 is :', v1)
+print(type(str1), type(str2), type(str3), type(str4))
+print(len(str1), len(str2), len(str3), len(str4))
 
+
+# 빈 문자열
+str1_t1 = ''
+str2_t2 = str()
+
+print(type(str1_t1), len(str1_t1))
+print(type(str2_t2), len(str2_t2))
+
+# 이스케이프 문자 사용
+"""
+참고 : Escape 코드
+
+\n : 개행
+\t : 탭
+\\ : 문자
+\' : 문자
+\" : 문자
+\000 : 널 문자
+...
+"""
+
+# I'm Boy
+print("I'm Boy")
+print('I\'m Boy')
+print('a \t b')
+print('a \"\" b')
+
+escape_str1 = "Do you have a \"retro games\"?"
+print(escape_str1)
+escape_str2 = 'What\'s on TV?'
+print(escape_str2)
+
+# 탭, 줄 바꿈
+t_s1 = "Click \t Start!"
+t_s2 = "New Line \n Check!"
+
+print(t_s1)
+print(t_s2)
 print()
 
-for v2 in range(1, 11): # 1 ~ 10
-    print('v2 is :', v2)
-
+# Raw String 출력
+raw_s1 = r'D:\Python\test'
+print(raw_s1)
 print()
 
-for v3 in range(0, 11, 2): # 1 ~ 10
-    print('v3 is :', v3)
+# 멀티라인 입력
+# 역슬래시 사용
+multi_str = \
+'''
+String
+Multi line
+Test
+'''
+
+print(multi_str)
+
+# 문자열 연산
+str_o1 = "Python"
+str_o2 = "Apple!"
+str_o3 = "How are you doing"
+str_o4 = "Seoul Daejeon Busan Jinju"
+
+print(str_o1 * 3)
+print(str_o1 + str_o2)
+print('y' in str_o1)
+print('n' in str_o1)
+print('p' not in str_o2)
+
+# 문자열 형 변환
+print(str(66), type(str(66)))
+print(str(10.1))
+print(str(True), type(str(True)))
+
+# 문자열 함수(upper, isalnum, startswith, count, endswith, isalpha...)
+print("Capitalize: ", str_o1.capitalize())
+print("endswith?: ", str_o2.endswith("!"))
+print("replace", str_o1.replace("thon", ' Good'))
+print("sorted: ", sorted(str_o1))
+print("split: ", str_o4.split(' '))
 
 
-# 1 ~ 1000 합
+# 반복(시퀀스)
+im_str = "Good Boy!"
 
-sum1 = 0
+print(dir(im_str)) # __iter__
 
-for v in range(1, 100):
-    sum1 += v
+# 출력
+for i in im_str:
+    print(i)
 
-print('1 ~ 1000 Sum : ', sum1)
+# 슬라이싱
+str_s1 = "Nice Python"
 
-print('1 ~ 1000 Sum : ', sum(range(1, 1001)))
-print('1 ~ 1000 4의 배수의 합 : ', sum(range(4, 1001, 4)))
+# 슬라이싱 연습
+print(str_s1[0:3]) # 0 1 2
+print(str_s1[5:]) # [5:11]
+print(str_s1[:len(str_s1)]) # str_s1[:11]
+print(str_s1[:len(str_s1)-1]) # str_s1[:10]
+print(str_s1[1:9:2])
+print(str_s1[-5:])
+print(str_s1[1:-2])
+print(str_s1[::2])
+print(str_s1[::-1])
 
-# Iterables 자료형 반복
-# 문자열, 리스트, 튜플, 집합, 사전(딕셔너리)
-# iterable 리턴 함수 : range, reversed, enumerate, filter, map, zip
+# 아스키 코드(또는 유니코드)
+a = 'z'
 
-# 예제1
-names = ['Kim', 'Park', 'Cho', 'Lee', 'Choi', 'Yoo']
+print(ord(a)) # 아스키 코드로
+print(chr(122)) # 문자로
 
-for n in names:
-    print('You are : ', n)
