1772deca078b2ad8af47dfa97e4dfef63f8fe615
SsangSoo <kss10080@naver.com>
2026-05-21 22:28:08 +0900

    python 입문 chapter06 - 람다 함수 심화
    함수 변수 할당, 람다 함수 변수 할당, 고차 함수(함수를 인수로 전달) 학습

---  introduction/chapter06/01.py
+++  introduction/chapter06/01.py
@@ -104,3 +104,25 @@ nested_func(100)
 # 람다는 즉시 실행 함수(Heap 초기화) -> 메모리 초기화
 # 남발 시 가독성 오히려 감소
 
+# def mul_func(x, y):
+#   return x * y
+
+#Lambda x, y:x*y
+
+# 일반적함수 -> 할당
+def mul_func(x, y):
+    return x * y
+
+print(mul_func(10, 50))
+
+mul_func_var = mul_func
+print(mul_func_var(20, 50))
+
+# 람다 함수 -> 할당
+lambda_mul_func = lambda x,y:x*y
+print(lambda_mul_func(50, 50))
+
+def func_final(x, y, func):
+    print('>>>>', x * y * func(100, 100))
+
+func_final(10, 20, mul_func_var)
